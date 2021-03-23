856a038b9ce744f67715a5d8a82bd2a52d64822a
jandanielcz <jandanielcz@gmail.com>
2021-03-23 05:42:05 +0100

    remove other uneeded files

--- make-plugin-dist.ps1
+++ make-plugin-dist.ps1
@@ -5,7 +5,8 @@ Remove-Item -Path $inside -Force -Recurse;
 Copy-Item -Path ./* -Destination $out -Recurse -Force;
 
 ## remove unwanted in dist zip
-$to_remove="node_modules",".git","README.md",".gitignore","package.json","package-lock.json", "webpack.config.js","make-plugin-dist.ps1";
+$to_remove="node_modules",".git","README.md",".gitignore","package.json","package-lock.json", "webpack.config.js","make-plugin-dist.ps1",
+    "media/font/specimen_files","media/font/generator_config.txt","media/font/proxima_nova_font-demo.html";
 foreach($name in $to_remove) {
     $a = Join-Path $out $name;
     Remove-Item -Path $a -Force -Recurse;
