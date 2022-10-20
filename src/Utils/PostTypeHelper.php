a6c8faaa17abea6a7121cd93bf39ee4566599296
Jiří Slischka <slischkaj@gmail.com>
2022-10-20 09:02:33 +0200

    Updated PostTypeHelper

--- src/Utils/PostTypeHelper.php
+++ src/Utils/PostTypeHelper.php
@@ -4,15 +4,14 @@ namespace FapiMember\Utils;
 
 class PostTypeHelper {
 
+	public static $excluded_post_type_prefixes = array( 'jet-', 'elementor_', 'elemental_' );
 
 	/**
-	 * @param bool
-	 * @return array
+	 * @param bool $cpt_only
+	 * @return array<string>
 	 */
 	public static function getSupportedPostTypes( $cpt_only = false ) {
-		$excluded_post_types         = $cpt_only ? array( 'attachment', 'page', 'post' ) : array( 'attachment' );
-		$excluded_post_type_prefixes = array( 'jet-', 'elementor_', 'elemental_' );
-
+		$excluded_post_types = $cpt_only ? array( 'attachment', 'page', 'post' ) : array( 'attachment' );
 		$supported_post_types_objects = get_post_types( array( 'public' => true ), 'objects', 'and' );
 
 		foreach ( $supported_post_types_objects as $obj ) {
@@ -22,7 +21,7 @@ class PostTypeHelper {
 				continue;
 			}
 
-			foreach ( $excluded_post_type_prefixes as $prefix ) {
+			foreach ( self::$excluded_post_type_prefixes as $prefix ) {
 				if ( strpos( $name, $prefix ) === 0 ) {
 					continue 2;
 				}
