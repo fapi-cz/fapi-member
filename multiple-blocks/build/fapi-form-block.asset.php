996300228eea5e1f73a4959ed3abcf8d37f968d5
Marek-Klein <marek.klein@fapi.cz>
2024-02-14 15:31:40 +0100

    Change name display method

--- multiple-blocks/build/fapi-form-block.asset.php
+++ multiple-blocks/build/fapi-form-block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8dfa98c91fc996107811');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '17fa3174ed811013ec2e');
